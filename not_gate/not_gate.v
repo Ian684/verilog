ab570f2c856df31b1af8ed786e0c0a2975878548
root <root@picoctf>
2026-09-09 21:06:55 +0800

    upload and_gate, or_gate, not_gate verilog pprojects

---
+++ not_gate/not_gate.v
@@ -0,0 +1,10 @@
+module not_gate(
+	input a,
+	input b,
+	input c,
+	output y
+);
+
+assign y = (a & b) | (~c);
+
+endmodule
---
+++ not_gate/not_gate_vb.v
@@ -0,0 +1,35 @@
+module not_gate_vb;
+
+reg a;
+reg b;
+reg c;
+wire y;
+
+integer i;
+
+not_gate uut(
+	.a(a),
+	.b(b),
+	.c(c),
+	.y(y)
+);
+
+initial begin 
+	
+	for (i = 0; i < 8; i = i + 1) begin
+		{a , b , c} = i;
+		#10;
+	end
+
+	$finish;
+end
+
+initial begin
+	$monitor(
+		"time=%0t , a=%b , b=%b , c=%b , y=%b",
+		$time , a , b , c , y	
+	);
+
+end
+
+endmodule
